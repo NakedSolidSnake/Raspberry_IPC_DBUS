1f604ec0458df15e17b6bb9d7aaa5069d3bf6989
Cristiano Silva <cristianosstec@gmail.com>
2020-11-30 17:27:59 -0300

    create launch

--- src/launch_processes.c
+++ src/launch_processes.c
@@ -1,61 +1,33 @@
-/**
- * @file led_process.c
- * @author Cristiano Silva de Souza (cristianosstec@gmail.com)
- * @brief Realiza a leitura da Shared Memory em modo
- * polling e aplica o estado ao led,
- * sendo seu estado alterado pelo processo de 
- * button_process
- * @version 1.0
- * @date 2020-02-09
- * 
- * @copyright Copyright (c) 2020
- * 
- */
-
-#include <led.h>
-#include <shm.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
 
-LED_t led =
-    {
-        .gpio.pin = 0,
-        .gpio.eMode = eModeOutput
-    };
-
-static SHMemory_t shm =
-    {
-    .path = "/shm_gpio",
-    .projId = 65,
-    .size = 1024,
-    .flags = 0666
-    };
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-    int state_cur;
-    int state_old;
+    int pid_button, pid_led;
+    int button_status, led_status;
 
-    if(LED_init(&led))
-        return EXIT_FAILURE;
+    pid_button = fork();
 
-    if(SHMemory_init(&shm))
-        return EXIT_FAILURE;
-
-    while(1)
+    if(pid_button == 0)
     {
-        sscanf(shm.shm, "state = %d", &state_cur);
-        if(state_cur !=  state_old)
-        {
+        //start button process
+        char *args[] = {"./button_process", NULL};
+        button_status = execvp(args[0], args);
+        printf("Error to start button process, status = %d\n", button_status);
+        abort();
+    }   
 
-            state_old = state_cur;
-            LED_set(&led, (eState_t)state_cur);            
-        }
-        usleep(1);
+    pid_led = fork();
+
+    if(pid_led == 0)
+    {
+        //Start led process
+        char *args[] = {"./led_process", NULL};
+        led_status = execvp(args[0], args);
+        printf("Error to start led process, status = %d\n", led_status);
+        abort();
     }
-
-    SHMemory_detach(&shm);    
 
     return EXIT_SUCCESS;
 }
